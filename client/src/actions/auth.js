669f7f68ca48109f1f6e316c2d67e844612c5e8c
Srinath N.S <srinath1@users.noreply.github.com>
2022-09-08 21:06:30 +0200

    Update auth.js

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-export const register=async(user) =>await axios.post(`https://myjuchotelbooking.herokuapp.com/register`,user)
-export const login=async(user) =>await axios.post(`https://myjuchotelbooking.herokuapp.com/login`,user);
+export const register=async(user) =>await axios.post(`https://myjuchotelbooking.herokuapp.com/api/register`,user)
+export const login=async(user) =>await axios.post(`https://myjuchotelbooking.herokuapp.com/api/login`,user);
 export const updateUserInLocalStorage = (user, next) => {
     if (window.localStorage.getItem("auth")) {
       let auth = JSON.parse(localStorage.getItem("auth"));
