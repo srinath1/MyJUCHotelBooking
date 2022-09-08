1f4311b22100ff3b2d7923d856b5ede77643675f
Srinath N.S <srinath1@users.noreply.github.com>
2022-09-08 20:43:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,10 +18,18 @@ mongoose
 
 // route middleware
 fs.readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+const path = require('path')
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
+if(process.env.NODE_ENV==='production')
+{
+
+    app.use('/' , express.static('client/build'))
+
+    app.get('*' , (req , res)=>{
+
+          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+
+    })
+
+}
 app.listen(port, () => console.log(`Server is running on port 8000`));
